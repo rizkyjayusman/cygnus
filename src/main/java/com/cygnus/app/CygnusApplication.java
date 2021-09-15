e0a87d03f597ed2b2dd419bed52e97697c47db2a
rizky <rizky.jayusman96@gmail.com>
2021-09-15 16:39:28 +0700

    init project

---
+++ src/main/java/com/cygnus/app/CygnusApplication.java
@@ -0,0 +1,13 @@
+package com.cygnus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CygnusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CygnusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cygnus/app/CygnusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cygnus.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CygnusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
